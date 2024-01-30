a23b1932a0b9e3e10d883367b4d1f25e13575d02
Daniel <daniel@siliconswamp.io>
2024-01-30 14:29:54 -0500

    Home page done

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+    target_text = "Daniel Taghavi, Full Stack Developer & Blockchain Engineer";
+    text = "";
+    i = 0;
+    
+    constructor() { 
+        // this.typeWriter();
+    }
 
+    ngAfterViewInit() {
+        setTimeout(this.typeWriter.bind(this), 500);
+    }
+
+    typeWriter() {
+
+        if (this.i < this.target_text.length) {
+            this.text = this.target_text.substring(0, this.i + 1);
+            this.i++;
+            setTimeout(this.typeWriter.bind(this), 100); // Adjust typing speed (100ms)
+        }
+    }
 }
